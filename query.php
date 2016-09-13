2820681fe3dc57fe2bd1ed7ad8c5010321b307f8
Maarten Dekkers <maartenn2001@gmail.com>
2016-09-13 17:32:31 +0200

    Create query.php

---
+++ query.php
@@ -0,0 +1,16 @@
+if ($query == "dit") {
+    $telegram->answerInlineQuery(array("inline_query_id" => $telegram->QueryID(), 'results' => json_encode($results)));
+
+	$results = array(
+		array(
+			"type" => "article", 
+			"id" => "1", 
+			"title" => "Title", 
+			"description" => "Description", 
+			"input_message_content" => array(
+				"message_text" => "<code>Message 1</code>", 
+				"parse_mode" => "HTML"
+			)
+		)
+	);
+}
