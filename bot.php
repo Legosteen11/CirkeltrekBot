42734c15289006f53bcaf71ae1c6759041ce2e30
Maarten Dekkers <maartenn2001@gmail.com>
2016-09-20 17:03:48 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -576,7 +576,7 @@ else if (strlen(strstr($text,"/giegantisch"))>0) {
 
 //facezoom
 else if (strlen(strstr($text,"/facezoom"))>0) {
-	$telegram->sendMessage(array('chat_id' => $chat_id, 'text' => facezoom()));
+	$telegram->sendSticker(array('chat_id' => $chat_id, 'sticker' => facezoom()));
 }
 
 else if ($telegram->update() != false) {
